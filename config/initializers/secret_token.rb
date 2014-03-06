ac80ebf9ad9330f1570f753b595458c0aa756017
Marcos Martins <marcos.martins2307@gmail.com>
2014-03-06 02:29:09 -0700

    Adds initial bootstrap layout to website. Adds further styling layouts using bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kellee::Application.config.secret_token = 'a0020c7984245d60c12ce619ccf68589b3377f542c195e1665bcdc62de897d30b5e0fd5beb6eaff71fc601c275f881ac3527c044cd729b681b41b4eadffafc95'
